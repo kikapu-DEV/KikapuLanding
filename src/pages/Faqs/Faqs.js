ee6d464a6fb2a9ca39a352d5b436b973a3e96f95
Diana-nyamai <dnyamai.dn@gmail.com>
2024-03-27 23:01:16 +0300

    update: Faqs section creation

--- src/pages/Faqs/Faqs.js
+++ src/pages/Faqs/Faqs.js
@@ -1,43 +1,42 @@
-// import React from "react";
-// import type { CollapseProps } from "antd";
-// import { Collapse } from "antd";
+import React from "react";
+import { Collapse } from "antd";
 
-// const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eligendi, odit fugiat dicta inventore atque tenetur earum vitae cum repellendus asperiores iste corporis, officiis incidunt adipisci facilis, quam quis consectetur!`;
+const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eligendi, odit fugiat dicta inventore atque tenetur earum vitae cum repellendus asperiores iste corporis, officiis incidunt adipisci facilis, quam quis consectetur!`;
 
-// const items: CollapseProps["items"] = [
-//   {
-//     key: "1",
-//     label: "Community-driven conservation",
-//     children: <p>{text}</p>,
-//   },
-//   {
-//     key: "2",
-//     label: "Sustainability",
-//     children: <p>{text}</p>,
-//   },
-//   {
-//     key: "3",
-//     label: "Collaboration and partnerships",
-//     children: <p>{text}</p>,
-//   },
-// ];
-// const Faqs = () => {
-//   return (
-//     <div className="faqsContainer">
-//       <div className="flex items-center gap-3 justify-center">
-//         <p className="w-20 border-b-2 border-orange"></p>
-//         <h2 className="text-center text-xl font-semibold">
-//           Guiding <span className="text-orange">Principles</span>{" "}
-//         </h2>
-//       </div>
+const items=[
+  {
+    key: "1",
+    label: "Community-driven conservation",
+    children: <p>{text}</p>,
+  },
+  {
+    key: "2",
+    label: "Sustainability",
+    children: <p>{text}</p>,
+  },
+  {
+    key: "3",
+    label: "Collaboration and partnerships",
+    children: <p>{text}</p>,
+  },
+];
+const Faqs = () => {
+  return (
+    <div className="faqsContainer">
+      <div className="container1">
+        <p className="container2"></p>
+        <h2 className="container3">
+          Guiding <span style={{ color: "#18D26E" }}>Principles</span>{" "}
+        </h2>
+      </div>
 
-//       <h1 className="text-center text-4xl font-bold mb-6">WHAT WE STAND FOR</h1>
+      <h1 className="header">WHAT WE STAND FOR</h1>
 
-//       <div className="lg:w-[70%] mx-auto">
-//         <Collapse accordion items={items} />
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="container4">
+        <Collapse accordion items={items} />
+      </div>
+    </div>
+  );
+};
 
-// export default Faqs;
+export default Faqs;
