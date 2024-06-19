f842faeaf9701e4cfce8fd73c3ed2a1329d0f23f
mundume <nzaih18@gmail.com>
2024-06-19 14:13:54 +0300

    make a few changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,41 @@
-import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { BiDownload } from "react-icons/bi";
 import { useRef } from "react";
 
 export const Navbar = () => {
-	const navRef = useRef();
+  const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsiveNav");
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsiveNav");
+  };
 
-	return (
-		<header>
-			<h2 className='logoName'>
-				Kikapuapp
-				{/* <span style={{ color: "#18D26E" }}>.</span> */}
-			</h2>
-			<nav ref={navRef}>
-				<Link to={"#"}>Home</Link>
-				<Link to={"#"}>About</Link>
-				<Link to={"#"}>Features</Link>
-				<Link to={"#"}>Team</Link>
-				<Link to={"#"}>Contact</Link>
-				<button className='downloadButtonSmall'>
-					Download <BiDownload className='downloadIcon' />{" "}
-				</button>
-				<button className='navBtn navCloseBtn' onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<div className='myDownloadButton'>
-				<button className='downloadButton'>Join waitlist</button>
-				{/* <button className="downloadButton">Download <BiDownload className="downloadIcon"/> </button> */}
-			</div>
+  return (
+    <header>
+      <h2 className="logoName">Kikapuapp</h2>
+      <nav ref={navRef}>
+        <a hrefLang="#">Home</a>
+        <a href={"#about"}>About</a>
+        <a href={"#features"}>Features</a>
+        <a href={"#team"}>Team</a>
+        <a href={"#contact"}>Contact</a>
+        <a
+          className="downloadButtonSmall"
+          href="https://play.google.com/store/apps/details?id=com.eldagenius.kikapustudents&hl=en-US&ah=9PoTnsKToiRY3oAU6-1IQbUEWVs"
+        >
+          Download <BiDownload className="downloadIcon" />{" "}
+        </a>
+        <button className="navBtn navCloseBtn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <div className="myDownloadButton">
+        <button className="downloadButton">Join waitlist</button>
+        {/* <button className="downloadButton">Download <BiDownload className="downloadIcon"/> </button> */}
+      </div>
 
-			<button className='navBtn' onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+      <button className="navBtn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 };
